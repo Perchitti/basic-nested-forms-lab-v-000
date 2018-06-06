ea7c2bcc4eb93c175e2655178aca1bd0e29d6ef5
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-06 16:16:29 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,9 +13,12 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-
+    if @recipe.save
     redirect_to recipe_path(@recipe)
+  else
+    render :new
   end
+end
 
 
   private
